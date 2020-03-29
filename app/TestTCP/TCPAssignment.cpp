616065b93f1873a61c861671bbb3169a9bd88cb4
Azamat Smagulov <s.a.smagulov@gmail.com>
2020-03-29 17:15:20 +0600

    Project 1 complete

--- app/TestTCP/TCPAssignment.cpp
+++ app/TestTCP/TCPAssignment.cpp
@@ -41,15 +41,80 @@ void TCPAssignment::finalize()
 
 }
 
+void TCPAssignment::syscall_socket(UUID syscallUUID, int pid, int type, int protocol) {
+	int fd = this->createFileDescriptor(pid);
+	if (fd != -1){
+		fds.insert(fd);
+	}
+	this->returnSystemCall(syscallUUID, fd);
+}
+
+void TCPAssignment::syscall_close(UUID syscallUUID, int pid, int sockfd) {
+	if (fds.find(sockfd) == fds.end()){
+		this->returnSystemCall(syscallUUID, -1);
+		return;
+	}
+
+	if (sockfdToAddrInfo.count(sockfd)) {
+		struct sockaddr addr = sockfdToAddrInfo[sockfd].first;
+
+		uint16_t port = ((struct sockaddr_in*) &addr)->sin_port;
+		uint32_t ip = ((struct sockaddr_in*) &addr)->sin_addr.s_addr;
+		
+		binded.erase({port, ip});
+		sockfdToAddrInfo.erase(sockfd);
+	}
+
+	fds.erase(sockfd);
+	this->removeFileDescriptor(pid, sockfd);
+	this->returnSystemCall(syscallUUID, 0);
+}
+
+void TCPAssignment::syscall_bind(
+	UUID syscallUUID, int pid, int sockfd,
+	struct sockaddr *addr, socklen_t addrlen) {
+	if (sockfdToAddrInfo.count(sockfd)) {
+		this->returnSystemCall(syscallUUID, -1);
+		return;
+	}
+
+	struct sockaddr copied_addr = *addr;
+
+	uint16_t port = ((struct sockaddr_in*) &copied_addr)->sin_port;
+	uint32_t ip = ((struct sockaddr_in*) &copied_addr)->sin_addr.s_addr;
+
+	if (binded.find({port, 0}) != binded.end() || binded.find({port, ip}) != binded.end()) {
+		this->returnSystemCall(syscallUUID, -1);
+		return;
+	}
+
+	sockfdToAddrInfo[sockfd] = {copied_addr, addrlen};
+	binded.insert({port, ip});
+
+	this->returnSystemCall(syscallUUID, 0);
+}
+
+void TCPAssignment::syscall_getsockname(
+	UUID syscallUUID, int pid, int sockfd,
+	struct sockaddr *addr, socklen_t* addrlen) {
+	if (!sockfdToAddrInfo.count(sockfd)) {
+		this->returnSystemCall(syscallUUID, -1);
+		return;
+	}
+	*addr = sockfdToAddrInfo[sockfd].first;
+	*addrlen = sockfdToAddrInfo[sockfd].second;
+	this->returnSystemCall(syscallUUID, 0);
+}
+
 void TCPAssignment::systemCallback(UUID syscallUUID, int pid, const SystemCallParameter& param)
 {
 	switch(param.syscallNumber)
 	{
 	case SOCKET:
-		//this->syscall_socket(syscallUUID, pid, param.param1_int, param.param2_int);
+		this->syscall_socket(syscallUUID, pid, param.param1_int, param.param2_int);
 		break;
 	case CLOSE:
-		//this->syscall_close(syscallUUID, pid, param.param1_int);
+		this->syscall_close(syscallUUID, pid, param.param1_int);
 		break;
 	case READ:
 		//this->syscall_read(syscallUUID, pid, param.param1_int, param.param2_ptr, param.param3_int);
@@ -70,14 +135,14 @@ void TCPAssignment::systemCallback(UUID syscallUUID, int pid, const SystemCallPa
 		//		static_cast<socklen_t*>(param.param3_ptr));
 		break;
 	case BIND:
-		//this->syscall_bind(syscallUUID, pid, param.param1_int,
-		//		static_cast<struct sockaddr *>(param.param2_ptr),
-		//		(socklen_t) param.param3_int);
+		this->syscall_bind(syscallUUID, pid, param.param1_int,
+				static_cast<struct sockaddr *>(param.param2_ptr),
+				(socklen_t) param.param3_int);
 		break;
 	case GETSOCKNAME:
-		//this->syscall_getsockname(syscallUUID, pid, param.param1_int,
-		//		static_cast<struct sockaddr *>(param.param2_ptr),
-		//		static_cast<socklen_t*>(param.param3_ptr));
+		this->syscall_getsockname(syscallUUID, pid, param.param1_int,
+				static_cast<struct sockaddr *>(param.param2_ptr),
+				static_cast<socklen_t*>(param.param3_ptr));
 		break;
 	case GETPEERNAME:
 		//this->syscall_getpeername(syscallUUID, pid, param.param1_int,
--- app/TestTCP/TCPAssignment.hpp
+++ app/TestTCP/TCPAssignment.hpp
@@ -15,7 +15,7 @@
 #include <netinet/tcp.h>
 #include <netinet/ip.h>
 #include <netinet/in.h>
-
+#include <set>
 
 #include <E/E_TimerModule.hpp>
 
@@ -28,13 +28,19 @@ private:
 
 private:
 	virtual void timerCallback(void* payload) final;
-
+	std::unordered_set<int> fds;
+	std::unordered_map<int, std::pair<struct sockaddr, socklen_t>> sockfdToAddrInfo;
+	std::unordered_set<std::pair<uint16_t, uint32_t>> binded;
 public:
 	TCPAssignment(Host* host);
 	virtual void initialize();
 	virtual void finalize();
 	virtual ~TCPAssignment();
 protected:
+	virtual void syscall_socket(UUID syscallUUID, int pid, int type, int protocol) final;
+	virtual void syscall_close(UUID syscallUUID, int pid, int sockfd) final;
+	virtual void syscall_bind(UUID syscallUUID, int pid, int sockfd, struct sockaddr *addr, socklen_t addrlen) final;
+	virtual void syscall_getsockname(UUID syscallUUID, int pid, int sockfd, struct sockaddr *addr, socklen_t* addrlen) final;
 	virtual void systemCallback(UUID syscallUUID, int pid, const SystemCallParameter& param) final;
 	virtual void packetArrived(std::string fromModule, Packet* packet) final;
 };
