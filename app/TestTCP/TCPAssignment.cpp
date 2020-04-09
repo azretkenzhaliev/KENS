f9e077ade3649889004fab0cbf38a0e5c7123f54
Azret Kenzhaliev <azret.kenzhaliev@gmail.com>
2020-04-09 20:59:42 +0600

    we did a great job - first 5 tests are passing

--- app/TestTCP/TCPAssignment.cpp
+++ app/TestTCP/TCPAssignment.cpp
@@ -44,21 +44,33 @@ void TCPAssignment::finalize()
 
 }
 
-void TCPAssignment::syscall_socket(UUID syscallUUID, int pid, int type, int protocol) {
-	// We assume that the createFileDescriptor function will always return a valid file descriptor.
+int TCPAssignment::_syscall_socket(UUID syscallUUID, int pid, int type, int protocol) {
 	int fd = this->createFileDescriptor(pid);
 
 	if (fd != -1) {
 		// If the file descriptor was successfully created make a note of that.
 		fds.insert(fd);
+
 		Azocket azocket;
+		azocket.pid = pid;
 		azocket.sockfd = fd;
+		azocket.syscall_id = syscallUUID;
 		azocket.state = sockstate::STATE_CLOSED;
+
+		std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+		azocket.seq_num = htonl(std::uniform_int_distribution<uint32_t>(0, UINT32_MAX)(rng));
+
+		azocket.ack_num = 0;
+
 		sockfdToAzocket[fd] = azocket;
 	}
 
 	// Return -1 or the created file descriptor.
-	this->returnSystemCall(syscallUUID, fd);
+	return fd;
+}
+
+void TCPAssignment::syscall_socket(UUID syscallUUID, int pid, int type, int protocol) {
+	this->returnSystemCall(syscallUUID, _syscall_socket(syscallUUID, pid, type, protocol));
 }
 
 void TCPAssignment::syscall_close(UUID syscallUUID, int pid, int sockfd) {
@@ -92,7 +104,7 @@ void TCPAssignment::syscall_close(UUID syscallUUID, int pid, int sockfd) {
 	this->returnSystemCall(syscallUUID, 0);
 }
 
-int TCPAssignment::bind(int sockfd, struct sockaddr *addr, socklen_t addrlen){
+int TCPAssignment::_syscall_bind(int sockfd, struct sockaddr *addr, socklen_t addrlen) {
 	// We decided to copy the given data (binding), so that
 	// in case if the data in the given pointer is ever
 	// freed without a notice, we'll be safe.
@@ -111,6 +123,9 @@ int TCPAssignment::bind(int sockfd, struct sockaddr *addr, socklen_t addrlen){
 	// If the checks passed, add (socket descriptor, binding) pair into the hash table
 	// and put the binding information into the set (hash table) of active bindings.
 	sockfdToAddrInfo[sockfd] = {copied_addr, addrlen};
+
+	IPPortToSockfd[{ip, port}] = sockfd;
+
 	binded.insert({port, ip});
 
 	sockfdToAzocket[sockfd].source_ip = ip;
@@ -127,7 +142,7 @@ void TCPAssignment::syscall_bind(
 		return;
 	}
 
-	this->returnSystemCall(syscallUUID, bind(sockfd, addr, addrlen));
+	this->returnSystemCall(syscallUUID, _syscall_bind(sockfd, addr, addrlen));
 }
 
 void TCPAssignment::syscall_getsockname(
@@ -149,43 +164,132 @@ void TCPAssignment::syscall_getsockname(
 
 void TCPAssignment::implicit_bind(int sockfd, uint32_t dest_ip) {
 	std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-	int port = std::uniform_int_distribution<int>(1025, 65535)(rng);
-	
-	uint8_t *local_ip;
-	int index = this->getHost()->getRoutingTable((uint8_t *)dest_ip);
-	this->getHost()->getIPAddr(local_ip, index);
+	uint16_t local_port = std::uniform_int_distribution<uint16_t>(1025, 65535)(rng);
+
+	uint8_t *dest_ip_by_8 = (uint8_t *) malloc(sizeof(uint32_t));
+
+	dest_ip_by_8[0] = (dest_ip >> 0) & 0xff;
+	dest_ip_by_8[1] = (dest_ip >> 8) & 0xff;
+	dest_ip_by_8[2] = (dest_ip >> 16) & 0xff;
+	dest_ip_by_8[3] = (dest_ip >> 24) & 0xff;
+
+	uint8_t *local_ip_by_8 = (uint8_t *) malloc(sizeof(uint32_t));
+	int index = this->getHost()->getRoutingTable(dest_ip_by_8);
+	this->getHost()->getIPAddr(local_ip_by_8, index);
+
+	uint32_t local_ip = (local_ip_by_8[0]
+		+ (local_ip_by_8[1] << 8)
+		+ (local_ip_by_8[2] << 16)
+		+ (local_ip_by_8[3] << 24));
+
+	local_ip = ntohl(local_ip);
+	local_port = ntohs(local_port);
 
 	struct sockaddr_in buf;
 	buf.sin_family = AF_INET;
-	buf.sin_addr.s_addr = (in_addr_t) *local_ip;
-	buf.sin_port = port;
+	buf.sin_addr.s_addr = local_ip;
+	buf.sin_port = local_port;
 	
 	struct sockaddr *local_addr = (struct sockaddr *) &buf;
 	socklen_t local_addrlen = sizeof(*local_addr);
 
-	while (bind(sockfd, local_addr, local_addrlen) != 0){
-		port = std::uniform_int_distribution<int>(1025, 65535)(rng);
-		buf.sin_port = port;
+	while (_syscall_bind(sockfd, local_addr, local_addrlen) != 0){
+		local_port = ntohs(std::uniform_int_distribution<int>(1025, 65535)(rng));
+		buf.sin_port = local_port;
 	}
 }
 
 void TCPAssignment::syscall_connect(
 	UUID syscallUUID,  int pid, int sockfd,
 	struct sockaddr *addr, socklen_t addrlen) {
-	uint32_t dest_ip = ((struct sockaddr_in*) &addr)->sin_addr.s_addr;
-	uint8_t dest_port = ((struct sockaddr_in*) &addr)->sin_port;
+	uint32_t dest_ip = ntohl(((struct sockaddr_in *) addr)->sin_addr.s_addr);
+	uint16_t dest_port = ntohs(((struct sockaddr_in *) addr)->sin_port);
 	implicit_bind(sockfd, dest_ip);
 
 	sockfdToAzocket[sockfd].dest_ip = dest_ip;
 	sockfdToAzocket[sockfd].dest_port = dest_port;
 
-	sockfdToAzocket[sockfd].sendSYNPacket(this); // sockfd, dest ip, ...
+	SipDip sipdip = getSipDip(sockfdToAzocket[sockfd].source_ip, sockfdToAzocket[sockfd].source_port, sockfdToAzocket[sockfd].dest_ip, sockfdToAzocket[sockfd].dest_port);
+	SipDipToSockfd[sipdip] = sockfd;
+
+	sendSYNPacket(sockfdToAzocket[sockfd]);
 
 	sockfdToAzocket[sockfd].syscall_id = syscallUUID;
 	sockfdToAzocket[sockfd].state = sockstate::STATE_SYNSENT;
 }
 
+void TCPAssignment::syscall_getpeername(UUID syscallUUID, int pid, int sockfd, 
+	struct sockaddr *addr, socklen_t* addrlen) {
 
+	struct sockaddr_in *buf = new struct sockaddr_in;
+	buf->sin_family = AF_INET;
+	buf->sin_addr.s_addr = htonl(sockfdToAzocket[sockfd].dest_ip);
+	buf->sin_port = htons(sockfdToAzocket[sockfd].dest_port);
+
+	*addr = *((struct sockaddr *) buf);
+	*addrlen = sizeof(*addr);
+
+	this->returnSystemCall(syscallUUID, 0);
+}
+
+Packet* TCPAssignment::makePacket(struct Azocket &azocket, const uint8_t flag) {
+	// 14 - Ethernet header
+	// 20 - IP header structure
+
+	Packet *packet = this->allocatePacket(54);
+
+	uint32_t source_ip = htonl(azocket.source_ip);
+	uint16_t source_port = htons(azocket.source_port);
+	uint32_t dest_ip = htonl(azocket.dest_ip);
+	uint16_t dest_port = htons(azocket.dest_port);
+	uint32_t seq_num = htonl(azocket.seq_num);
+	uint32_t ack_num = htonl(azocket.ack_num);
+
+	// azocket.seq_num++;
+	// uint32_t ack_num = azocket.ack_num;
+
+	uint16_t total_length = htons(20);
+	packet->writeData(14 + 2, &total_length, 2);
+
+	packet->writeData(14 + 12, &source_ip, 4);
+	packet->writeData(14 + 16, &dest_ip, 4);
+	packet->writeData(14 + 20 + 0, &source_port, 2);
+	packet->writeData(14 + 20 + 2, &dest_port, 2);
+	packet->writeData(14 + 20 + 4, &seq_num, 4);
+	packet->writeData(14 + 20 + 8, &ack_num, 4);
+
+	uint8_t data_offset = 5 << 4;
+	packet->writeData(14 + 20 + 12, &data_offset, 1);
+
+	packet->writeData(14 + 20 + 13, &flag, 1);
+
+	uint16_t window_size = htons(51200);
+	packet->writeData(14 + 20 + 14, &window_size, 2);
+
+	size_t tcp_len = 20;
+	uint8_t *tcp_seg = (uint8_t *) malloc(tcp_len);
+	packet->readData(14 + 20, tcp_seg, tcp_len);
+
+	uint16_t checksum = htons(~NetworkUtil::tcp_sum(source_ip, dest_ip, tcp_seg, tcp_len));
+	packet->writeData(14 + 20 + 16, &checksum, 2);
+
+	return packet;
+}
+
+void TCPAssignment::sendSYNPacket(struct Azocket &azocket) {
+	Packet *packet = makePacket(azocket, packetTypes::SYN);
+	this->sendPacket("IPv4", packet);
+}
+
+void TCPAssignment::sendSYNACKPacket(struct Azocket &azocket) {
+	Packet *packet = makePacket(azocket, packetTypes::SYNACK);
+	this->sendPacket("IPv4", packet);
+}
+
+void TCPAssignment::sendACKPacket(struct Azocket &azocket) {
+	Packet *packet = makePacket(azocket, packetTypes::ACK);
+	this->sendPacket("IPv4", packet);
+}
 
 void TCPAssignment::systemCallback(UUID syscallUUID, int pid, const SystemCallParameter& param)
 {
@@ -204,8 +308,8 @@ void TCPAssignment::systemCallback(UUID syscallUUID, int pid, const SystemCallPa
 		//this->syscall_write(syscallUUID, pid, param.param1_int, param.param2_ptr, param.param3_int);
 		break;
 	case CONNECT:
-		// this->syscall_connect(syscallUUID, pid, param.param1_int,
-		// 		static_cast<struct sockaddr*>(param.param2_ptr), (socklen_t)param.param3_int);
+		 this->syscall_connect(syscallUUID, pid, param.param1_int,
+		 		static_cast<struct sockaddr*>(param.param2_ptr), (socklen_t)param.param3_int);
 		break;
 	case LISTEN:
 		//this->syscall_listen(syscallUUID, pid, param.param1_int, param.param2_int);
@@ -226,24 +330,93 @@ void TCPAssignment::systemCallback(UUID syscallUUID, int pid, const SystemCallPa
 				static_cast<socklen_t*>(param.param3_ptr));
 		break;
 	case GETPEERNAME:
-		//this->syscall_getpeername(syscallUUID, pid, param.param1_int,
-		//		static_cast<struct sockaddr *>(param.param2_ptr),
-		//		static_cast<socklen_t*>(param.param3_ptr));
+		this->syscall_getpeername(syscallUUID, pid, param.param1_int,
+				static_cast<struct sockaddr *>(param.param2_ptr),
+				static_cast<socklen_t*>(param.param3_ptr));
 		break;
 	default:
 		assert(0);
 	}
 }
 
-void TCPAssignment::packetArrived(std::string fromModule, Packet* packet)
-{
-	
+uint8_t TCPAssignment::getFlags(Packet *packet) {
+	uint8_t flags = 0;
+	packet->readData(14 + 20 + 13, &flags, 1);
+	return flags;
 }
 
-void TCPAssignment::timerCallback(void* payload)
-{
-
+TCPAssignment::SipDip TCPAssignment::getSipDip(uint32_t source_ip, uint16_t source_port, uint32_t dest_ip, uint16_t dest_port) {
+	return SipDip(source_ip, source_port, dest_ip, dest_port);
 }
 
+void TCPAssignment::packetArrived(std::string fromModule, Packet *packet) {
+	uint8_t flags = getFlags(packet);
 
+	uint32_t source_ip = 0;
+	uint16_t source_port = 0;
+	uint32_t dest_ip = 0;
+	uint16_t dest_port = 0;
+	uint32_t seq_num = 0;
+
+	packet->readData(14 + 12, &source_ip, 4);
+	packet->readData(14 + 16, &dest_ip, 4);
+	packet->readData(14 + 20 + 0, &source_port, 2);
+	packet->readData(14 + 20 + 2, &dest_port, 2);
+	packet->readData(14 + 20 + 4, &seq_num, 4);
+
+	freePacket(packet);
+
+	source_ip = ntohl(source_ip);
+	source_port = ntohs(source_port);
+	dest_ip = ntohl(dest_ip);
+	dest_port = ntohs(dest_port);
+	seq_num = ntohl(seq_num);
+
+	int sockfd;
+	SipDip sipdip;
+
+	switch ((uint16_t) flags) {
+		case packetTypes::FIN: {
+			break;
+		}
+		case packetTypes::SYN: { // server accepts connection
+			sockfd = IPPortToSockfd[{dest_ip, dest_port}];
+			int new_sockfd = _syscall_socket(sockfdToAzocket[sockfd].syscall_id, sockfdToAzocket[sockfd].pid, 0, 0);
+
+			sockfdToAzocket[new_sockfd].source_ip = dest_ip;
+			sockfdToAzocket[new_sockfd].source_port = dest_port;
+			sockfdToAzocket[new_sockfd].dest_ip = source_ip;
+			sockfdToAzocket[new_sockfd].dest_port = source_port;
+
+			sipdip = getSipDip(source_ip, source_port, dest_ip, dest_port);
+			SipDipToSockfd[sipdip] = new_sockfd;
+			
+			sendSYNACKPacket(sockfdToAzocket[new_sockfd]);
+
+			sockfdToAzocket[new_sockfd].state = sockstate::STATE_SYN_RCVD;
+			break;
+		}
+		case packetTypes::SYNACK: { // client established connection
+			sipdip = getSipDip(source_ip, source_port, dest_ip, dest_port);
+			sockfd = SipDipToSockfd[sipdip];
+			
+			sockfdToAzocket[sockfd].ack_num = seq_num + 1;
+			sendACKPacket(sockfdToAzocket[sockfd]);
+
+			sockfdToAzocket[sockfd].state = sockstate::STATE_ESTAB;
+			this->returnSystemCall(sockfdToAzocket[sockfd].syscall_id, 0);
+			break;
+		}
+		case packetTypes::ACK: {
+			sipdip = getSipDip(source_ip, source_port, dest_ip, dest_port);
+			sockfd = SipDipToSockfd[sipdip];
+			sockfdToAzocket[sockfd].state = sockstate::STATE_ESTAB;
+			break;
+		}
+	}
+}
+
+void TCPAssignment::timerCallback(void* payload) {
+
+}
 }
--- app/TestTCP/TCPAssignment.hpp
+++ app/TestTCP/TCPAssignment.hpp
@@ -42,6 +42,8 @@ protected:
 	// It is the main data structure used for the effective
 	// socket descriptor <-> port/ip translation.
 	std::unordered_map<int, std::pair<struct sockaddr, socklen_t>> sockfdToAddrInfo;
+	
+	std::unordered_map<std::pair<uint32_t, uint16_t>, int> IPPortToSockfd;
 
 	// binded is a hash table, which stores the active port/ip bindings.
 	// It is the main data structure used for binding collision checking.
@@ -55,83 +57,91 @@ protected:
 		STATE_ESTAB
 	} sockstate;
 
-	// Azocket:
-	// - source ip
-	// - source port
-	// - dest ip
-	// - dest port
-	// - sockfd
-	// - seq num
-	// - syscall ID (UUID)
-	// - backlog
-	// - state
 	struct Azocket {
 		uint32_t source_ip;
 		uint16_t source_port;
 		uint32_t dest_ip;
 		uint16_t dest_port;
+
+		int pid;
 		int sockfd;
+
 		uint32_t seq_num;
+		uint32_t ack_num;
+
 		UUID syscall_id;
+
 		int backlog;
+
 		sockstate state;
-
-		void sendSYNPacket(TCPAssignment *context){
-			// 12 - Ethernet header
-			// 20 - IP header structure
-
-			Packet *packet = context->allocatePacket(34);
-			packet->writeData(14+12, (void *) source_ip, 4);
-			packet->writeData(14+16, (void *) dest_ip, 4);
-			packet->writeData(14+20+0, (void *) source_port, 2);
-			packet->writeData(14+20+2, (void *) dest_port, 2);
-			int flag = 
-			packet->writeData(14+20+13, 1 << 1, 1);
-
-			context->sendPacket("IPv4", packet);
-		}
-
-		// void operator = (Azocket _azocket) {
-		// 	source_ip = _azocket.source_ip;
-		// 	source_port = _azocket.source_port;
-		// 	dest_ip = _azocket.dest_ip;
-		// 	dest_port = _azocket.dest_port;
-		// 	sockfd = _azocket.sockfd;
-		// 	seq_num = _azocket.seq_num;
-		// 	syscall_id = _azocket.syscall_id;
-		// 	backlog = _azocket.backlog;
-		// 	state = _azocket.state;
-		// }
 	};
 
+
+
 	// map: int (sockfd) -> Azocket
-	std::unordered_map<int, Azocket> sockfdToAzocket;
+	std::unordered_map<int, struct Azocket> sockfdToAzocket;
 
 	struct SipDip {
-		struct sockaddr s_addr;
-		socklen_t s_addrlen; 
-		struct sockaddr d_addr;
-		socklen_t d_addrlen;
+		uint32_t source_ip;
+		uint16_t source_port;
+		uint32_t dest_ip;
+		uint16_t dest_port;
+
+		SipDip() {
+			source_ip = source_port = 0;
+			dest_ip = dest_port = 0;
+		}
+		SipDip(uint32_t _source_ip, uint16_t _source_port, uint32_t _dest_ip, uint16_t _dest_port) {
+			source_ip = _source_ip;
+			source_port = _source_port;
+			dest_ip = _dest_ip;
+			dest_port = _dest_port;
+		}
+
+		friend const bool operator < (SipDip f, SipDip s) {
+			return f.source_ip < s.source_ip || (f.source_ip == s.source_ip && f.source_port < s.source_port);
+		}
 	};
 	
 	// map: SipDip -> sockfd
-	std::map<struct SipDip, int> SipDipTosockfd;
+	std::map<struct SipDip, int> SipDipToSockfd;
 
 	typedef enum {
-		SYN,
-		ACK,
-		SYNACK
-	} packetType;
+		FLAG_FIN,
+		FLAG_SYN,
+		FLAG_RST,
+		FLAG_PSH,
+		FLAG_ACK,
+		FLAG_URG,
+		FLAG_ECE,
+		FLAG_CWR
+	} packetFlags;
 
+	typedef enum {
+		FIN = (1 << packetFlags::FLAG_FIN),
+		SYN = (1 << packetFlags::FLAG_SYN),
+		SYNACK = (1 << packetFlags::FLAG_SYN) | (1 << packetFlags::FLAG_ACK),
+		ACK = (1 << packetFlags::FLAG_ACK)
+	} packetTypes;
+
+	virtual int _syscall_socket(UUID syscallUUID, int pid, int type, int protocol) final;
 	virtual void syscall_socket(UUID syscallUUID, int pid, int type, int protocol) final;
 	virtual void syscall_close(UUID syscallUUID, int pid, int sockfd) final;
-	virtual int bind(int sockfd, struct sockaddr *addr, socklen_t addrlen) final;
-	virtual void implicit_bind(int sockfd, uint32_t dest_ip) final;
+	virtual int _syscall_bind(int sockfd, struct sockaddr *addr, socklen_t addrlen) final;
 	virtual void syscall_bind(UUID syscallUUID, int pid, int sockfd, struct sockaddr *addr, socklen_t addrlen) final;
 	virtual void syscall_getsockname(UUID syscallUUID, int pid, int sockfd, struct sockaddr *addr, socklen_t* addrlen) final;
 	virtual void syscall_connect(UUID syscallUUID,  int pid, int sockfd, struct sockaddr *addr, socklen_t addrlen) final;
+	virtual void syscall_getpeername(UUID syscallUUID, int pid, int sockfd, struct sockaddr *addr, socklen_t* addrlen) final;
 	virtual void systemCallback(UUID syscallUUID, int pid, const SystemCallParameter& param) final;
 	virtual void packetArrived(std::string fromModule, Packet* packet) final;
+
+	virtual void implicit_bind(int sockfd, uint32_t dest_ip) final;
+	virtual uint8_t getFlags(Packet *packet) final;
+	virtual Packet* makePacket(struct Azocket &azocket, const uint8_t flag) final;
+	virtual void sendSYNPacket(struct Azocket &azocket) final;	
+	virtual void sendSYNACKPacket(struct Azocket &azocket) final;
+	virtual void sendACKPacket(struct Azocket &azocket) final;
+	virtual SipDip getSipDip(uint32_t source_ip,uint16_t source_port, uint32_t dest_ip,uint16_t dest_port) final;
 };
 
 class TCPAssignmentProvider
